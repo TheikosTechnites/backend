82d1a21bceca8472462fe27368fa36caa905d15c
John Neil Cotacte <johnneilcotacte@Johns-MacBook-Air.local>
2024-06-25 19:02:27 +0800

    Update

--- scripts/init-repository.sh
+++ scripts/init-repository.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# # Initialize Git config
-# if [ -d '.git' ]
-# then
-#   cat .gitconfig >> .git/config
-# elif [ -f '.git' ]
-# then
-#   read -ra CONFIG <<< $(cat .git)
-#   cat .gitconfig >> "${CONFIG[1]}/config"
-# fi
+# Initialize Git config
+if [ -d '.git' ]
+then
+  cat .gitconfig >> .git/config
+elif [ -f '.git' ]
+then
+  read -ra CONFIG <<< $(cat .git)
+  cat .gitconfig >> "${CONFIG[1]}/config"
+fi
 
 # Install required dependencies
 npm ci
