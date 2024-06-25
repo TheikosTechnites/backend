f4b5eeee62fe62fd959c0130241c08e45b67f018
John Neil Cotacte <johnneilcotacte@Johns-MacBook-Air.local>
2024-06-26 07:05:35 +0800

    Temporarily update env vars and readme

--- src/config/index.env.ts
+++ src/config/index.env.ts
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const ENABLE_TEST = process.env.ENABLE_TEST === '';
-const ENABLE_BUYBACK_V2 = process.env.ENABLE_BUYBACK_V2 === 'true';
+const ENVIRONMENT = process.env.ENVIRONMENT === '';
 
-export { ENABLE_BUYBACK_V2, ENABLE_TEST };
+export { ENVIRONMENT };
