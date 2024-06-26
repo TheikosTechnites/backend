071c84247ac3b28c1455bb711f14a416d5247cc7
John Neil Cotacte <johnneilcotacte@Johns-MacBook-Air.local>
2024-06-26 17:27:46 +0800

    Update

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,15 +4,18 @@ import { AppUseCase } from './v1/usecase/app.usecase';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PolicyModule } from '../policy/policy.module';
+import { MONGODB_URI } from 'src/config/index.env';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot(MONGODB_URI),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: true,
     }),
+    PolicyModule,
   ],
   controllers: [AppController],
   providers: [AppUseCase],
--- src/modules/policy/usecase/policy.usecase.ts
+++ src/modules/policy/usecase/policy.usecase.ts
@@ -4,6 +4,7 @@ import {
   IPolicyRepositoryToken,
 } from './interface/policy-repository.interface';
 import { PolicyModel } from '../entity/policy.model';
+import { CreatePolicyInputModel } from '../entity/create-policy.input.model';
 
 @Injectable()
 export class PolicyUsecase {
@@ -12,8 +13,8 @@ export class PolicyUsecase {
     private readonly privacyPolicyRepository: IPolicyRepository,
   ) {}
 
-  async createPolicy(): Promise<PolicyModel> {
-    return this.privacyPolicyRepository.getPrivacyPolicy();
+  async createPolicy(input: CreatePolicyInputModel): Promise<PolicyModel> {
+    return this.privacyPolicyRepository.createPolicy(input);
   }
   async getPrivacyPolicy(): Promise<PolicyModel> {
     return this.privacyPolicyRepository.getPrivacyPolicy();
